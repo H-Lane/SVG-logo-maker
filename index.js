71f83a261e91bb576e61a7ed4e20e39f4c2c5da7
Hersey <helane1996@gmail.com>
2024-05-09 22:17:27 -0400

    modularized the project by putting the writetofile function in its own file in the lib folder

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //Import necessary packages and SVG shape rendering page
 const inquirer = require(`inquirer`);
-const fs = require(`fs`);
-const { Circle, Square, Triangle } = require(`./lib/shapes`);
+const writeToFile = require(`./lib/svgrender`);
 
 function init() {
   inquirer
@@ -36,29 +35,6 @@ function init() {
         init();
       } else writeToFile("logo.svg", response);
     });
-}
-function writeToFile(name, userInput) {
-  let svgCode = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    viewBox="0 0 400 400" style="enable-background:new 0 0 400 400;" xml:space="preserve">`;
-
-  if (userInput.shape === "Circle") {
-    const shape = new Circle(userInput.shapeColor);
-    svgCode += shape.render();
-  } else if (userInput.shape === "Square") {
-    const shape = new Square(userInput.shapeColor);
-    svgCode += shape.render();
-  } else {
-    const shape = new Triangle(userInput.shapeColor);
-    svgCode += shape.render();
-  };
-
-  svgCode += `<text x="150" y="155" dominant-baseline="middle" text-anchor="middle" font-size="50" fill="${userInput.textColor}">${userInput.text}</text>`;
-
-  svgCode += `</svg>`;
-
-  fs.writeFile(name, svgCode, (err) => {
-    err ? console.log(err) : console.log("Generated logo.svg");
-  });
-}
+};
 
 init();
